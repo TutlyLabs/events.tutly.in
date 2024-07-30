f2ebf3c244c1ecc84beef0799a05fa2c69fb074a
vighnesh-7 <vangarivighnesh@gmail.com>
2024-07-31 03:08:12 +0530

    db push

--- src/app/api/upcoming-contests/[platform]/route.ts
+++ src/app/api/upcoming-contests/[platform]/route.ts
@@ -1,3 +1,4 @@
+import { requestContestsByPlatform } from "@/actions/contests";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
@@ -11,14 +12,10 @@ export async function GET(
   }
 ) {
 
-  const url = `https://clist.by:443/api/v4/contest/?username=vvlegend&api_key=e1dbb88caacaa4b12eca169bc3e60eac39048874&upcoming=true&start__gte=${new Date().toISOString()}&order_by=start&host=${params.platform}`;
 
   try {
-    const res = await fetch(url);
-    let data = await res.json();
-
-    const newData = data.objects;
-    return NextResponse.json(newData);
+    const res = await requestContestsByPlatform(params.platform);
+    return NextResponse.json(res);
   } catch (error: any) {
     return NextResponse.json({
       error: error.message,
--- src/app/api/upcoming-contests/route.ts
+++ src/app/api/upcoming-contests/route.ts
@@ -1,22 +1,12 @@
+import { requestAllContests } from "@/actions/contests";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  const url = `https://clist.by:443/api/v4/contest/?username=vvlegend&api_key=e1dbb88caacaa4b12eca169bc3e60eac39048874&upcoming=true&start__gte=${new Date().toISOString()}&order_by=start`;
 
   try {
-    const res = await fetch(url);
-    let data = await res.json();
+    const res = await requestAllContests();
 
-    const newData = data.objects.filter(
-      (contest: any) =>
-        contest.host === "codeforces.com" ||
-        contest.host === "codechef.com" ||
-        contest.host === "leetcode.com"
-    );
-
-    
-    
-    return NextResponse.json(newData);
+    return NextResponse.json(res);
   } catch (error: any) {
     return NextResponse.json({
       error: error.message,
