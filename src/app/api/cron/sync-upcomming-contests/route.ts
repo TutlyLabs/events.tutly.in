49e2b106ffa50a8068ff89ea54d47f676a9af8eb
Uday Sagar <udaysagar.mail@gmail.com>
2024-07-31 16:49:17 +0530

    changes to get request

--- src/app/api/cron/sync-upcomming-contests/route.ts
+++ src/app/api/cron/sync-upcomming-contests/route.ts
@@ -1,7 +1,7 @@
 import { fetchAllContests } from "@/actions/contests";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextRequest) {
+export async function GET(req: NextRequest) {
 
   try {
     const res = await fetchAllContests();
