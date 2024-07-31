6bef8630ee2edcd57df8449ea2449ec4ff3ff38a
Uday Sagar <udaysagar.mail@gmail.com>
2024-07-31 14:54:25 +0530

    fix cron redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,7 +15,7 @@ export default auth((req) => {
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
   const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
-  const isCronRoute = nextUrl.pathname.startsWith("/cron");
+  const isCronRoute = nextUrl.pathname.startsWith("/api/cron");
 
   if (isApiAuthRoute || isCronRoute) {
     return;
