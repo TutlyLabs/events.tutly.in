df868a17a8c28431fc0c485d0d8ce5363ae131e6
goutham4126 <goutham4126@gmail.com>
2024-08-23 00:27:23 +0530

    changes

--- src/components/Description.tsx.tsx
+++ src/components/Description.tsx.tsx
@@ -131,7 +131,6 @@ function Description({ event }: { event: any }) {
                 />
               </div>
             </div>
-
             {/* Modal for sharing */}
             {modal && (
               <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
